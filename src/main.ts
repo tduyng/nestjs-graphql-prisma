99e14bf5888de0450134cfa43a8f9778e669c4a8
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-02-01 19:49:14 +0100

    Add more options for app

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { setupSwagger } from '@common/configs/swagger-options';
 import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Validation
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+  app.use(cookieParser());
+  app.use(helmet());
 
   setupSwagger(app);
   const port = Number(process.env.SERVER_PORT);
