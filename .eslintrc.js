1ab729e8fa68bfd33973c788595232dd4994f0e7
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-02-02 18:22:55 +0100

    Exluce test folder from eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js', 'node_modules'],
+  ignorePatterns: ['.eslintrc.js', 'node_modules', 'test'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
