cff8e73bf7b07911692ca964e396418286b5adf7
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-02-03 10:07:45 +0100

    Update seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,39 +9,112 @@ async function main() {
 
   await prisma.$connect();
 
-  await prisma.category.deleteMany({});
-  await prisma.post.deleteMany({});
-  await prisma.profile.deleteMany({});
-  await prisma.user.deleteMany({});
+  /*********************************************************************/
+  /* Dangerous zone: */
+  /* Delete all tables before seeding */
+  // Only uncomment these block code below if you want to delete for all existed tables
+
+  // await prisma.category.deleteMany({});
+  // await prisma.post.deleteMany({});
+  // await prisma.profile.deleteMany({});
+  // await prisma.user.deleteMany({});
+
+  /*********************************************************************/
 
   /* Category seeding */
-  const category1 = await prisma.category.create({
-    data: {
+  const category1 = await prisma.category.upsert({
+    where: { slug: 'programing' },
+    create: {
       name: 'Programing',
       slug: 'programing',
     },
+    update: {
+      name: 'Programing',
+    },
   });
 
-  const category2 = await prisma.category.create({
-    data: {
+  const category2 = await prisma.category.upsert({
+    where: { slug: 'web-development' },
+    create: {
       name: 'Web development',
       slug: 'web-development',
+    },
+    update: {
+      name: 'Web development',
     },
   });
 
   // password: 1234567
 
-  const user1 = await prisma.user.create({
-    data: {
+  const user1 = await prisma.user.upsert({
+    where: { email: 'tester-user1@example.com' },
+    create: {
       email: 'tester1@example.com',
-      username: 'tester1',
-      password: '$2y$10$hd/JLwBF16K9QNgXdlbzbOgCaGjYrRtQIiTGwxo6U7tA8XaWgvhAq ',
+      username: 'tester-user1',
+      password: '$2y$10$hd/JLwBF16K9QNgXdlbzbOgCaGjYrRtQIiTGwxo6U7tA8XaWgvhAq',
       role: 'USER',
       profile: {
         create: {
-          firstName: 'Tester',
-          lastName: '1',
+          firstName: 'User 1',
+          lastName: 'Tester',
           bio: 'Web developer',
+        },
+      },
+    },
+    update: {
+      email: 'tester1@example.com',
+      username: 'tester1',
+      password: '$2y$10$hd/JLwBF16K9QNgXdlbzbOgCaGjYrRtQIiTGwxo6U7tA8XaWgvhAq',
+      role: 'USER',
+      profile: {
+        upsert: {
+          create: {
+            firstName: 'User 1',
+            lastName: 'Tester',
+            bio: 'Web developer',
+          },
+          update: {
+            firstName: 'User 1',
+            lastName: 'Tester',
+            bio: 'Web developer',
+          },
+        },
+      },
+    },
+  });
+
+  const user2 = await prisma.user.upsert({
+    where: { email: 'tester-user2@example.com' },
+    create: {
+      email: 'tester-user2@example.com',
+      username: 'tester-user2',
+      password: '$2y$10$hd/JLwBF16K9QNgXdlbzbOgCaGjYrRtQIiTGwxo6U7tA8XaWgvhAq',
+      role: 'USER',
+      profile: {
+        create: {
+          firstName: 'User 2',
+          lastName: 'Tester',
+          bio: 'Web developer',
+        },
+      },
+    },
+    update: {
+      email: 'tester-user2@example.com',
+      username: 'tester-user2',
+      password: '$2y$10$hd/JLwBF16K9QNgXdlbzbOgCaGjYrRtQIiTGwxo6U7tA8XaWgvhAq',
+      role: 'USER',
+      profile: {
+        upsert: {
+          create: {
+            firstName: 'User 2',
+            lastName: 'Tester',
+            bio: 'Web developer',
+          },
+          update: {
+            firstName: 'User 2',
+            lastName: 'Tester',
+            bio: 'Web developer',
+          },
         },
       },
     },
@@ -49,35 +122,61 @@ async function main() {
 
   // password: 1234567
 
-  const user2 = await prisma.user.create({
-    data: {
-      email: 'admin-tester1@example.com',
-      username: 'admin-tester1',
-      password: '$2y$10$hd/JLwBF16K9QNgXdlbzbOgCaGjYrRtQIiTGwxo6U7tA8XaWgvhAq ',
+  const user3 = await prisma.user.upsert({
+    where: { email: 'tester-admin1@example.com' },
+    create: {
+      email: 'tester-admin1@example.com',
+      username: 'tester-admin1',
+      password: '$2y$10$hd/JLwBF16K9QNgXdlbzbOgCaGjYrRtQIiTGwxo6U7tA8XaWgvhAq',
       role: 'ADMIN',
       profile: {
         create: {
-          firstName: 'Admin',
-          lastName: 'Site',
+          firstName: 'Admin 1',
+          lastName: 'Tester',
           bio: 'Fullstack developer',
+        },
+      },
+    },
+    update: {
+      username: 'tester-admin1',
+      password: '$2y$10$hd/JLwBF16K9QNgXdlbzbOgCaGjYrRtQIiTGwxo6U7tA8XaWgvhAq',
+      role: 'ADMIN',
+      profile: {
+        upsert: {
+          create: {
+            firstName: 'Admin 1',
+            lastName: 'Tester',
+            bio: 'Fullstack developer',
+          },
+          update: {
+            firstName: 'Admin 1',
+            lastName: 'Tester',
+            bio: 'Fullstack developer',
+          },
         },
       },
     },
   });
 
   /* Post seeding */
-
-  const post1 = await prisma.post.create({
-    data: {
+  //Post 1
+  await prisma.post.upsert({
+    where: { slug: 'join-us-for-prisma-day-2021' },
+    create: {
       title: 'Join us for Prisma Day 2021',
       slug: 'join-us-for-prisma-day-2021',
       content: 'https://www.prisma.io/day/',
       published: true,
+      author: { connect: { id: user1.id } },
+      categories: {
+        connect: [{ id: category1.id }, { id: category2.id }],
+      },
     },
-  });
-  await prisma.post.update({
-    where: { id: post1.id },
-    data: {
+    update: {
+      title: 'Join us for Prisma Day 2021',
+      slug: 'join-us-for-prisma-day-2021',
+      content: 'https://www.prisma.io/day/',
+      published: true,
       author: { connect: { id: user1.id } },
       categories: {
         connect: [{ id: category1.id }, { id: category2.id }],
@@ -85,17 +184,24 @@ async function main() {
     },
   });
 
-  const post2 = await prisma.post.create({
-    data: {
+  //Post 2
+  await prisma.post.upsert({
+    where: { slug: 'subscribe-to-graphql-weekly-for-community-news' },
+    create: {
       title: 'Subscribe to GraphQL Weekly for community news',
       slug: 'subscribe-to-graphql-weekly-for-community-news',
       content: 'https://graphqlweekly.com/',
       published: true,
+      author: { connect: { id: user2.id } },
+      categories: {
+        connect: [{ id: category1.id }, { id: category2.id }],
+      },
     },
-  });
-  await prisma.post.update({
-    where: { id: post2.id },
-    data: {
+    update: {
+      title: 'Subscribe to GraphQL Weekly for community news',
+      slug: 'subscribe-to-graphql-weekly-for-community-news',
+      content: 'https://graphqlweekly.com/',
+      published: true,
       author: { connect: { id: user2.id } },
       categories: {
         connect: [{ id: category1.id }, { id: category2.id }],
@@ -103,17 +209,24 @@ async function main() {
     },
   });
 
-  const post3 = await prisma.post.create({
-    data: {
+  // Post 3
+  await prisma.post.upsert({
+    where: { slug: 'follow-prisma-on-twitter' },
+    create: {
       title: 'Follow Prisma on Twitter',
       slug: 'follow-prisma-on-twitter',
       content: 'https://twitter.com/prisma',
       published: true,
+      author: { connect: { id: user2.id } },
+      categories: {
+        connect: [{ id: category1.id }, { id: category2.id }],
+      },
     },
-  });
-  await prisma.post.update({
-    where: { id: post3.id },
-    data: {
+    update: {
+      title: 'Follow Prisma on Twitter',
+      slug: 'follow-prisma-on-twitter',
+      content: 'https://twitter.com/prisma',
+      published: true,
       author: { connect: { id: user2.id } },
       categories: {
         connect: [{ id: category1.id }, { id: category2.id }],
@@ -121,8 +234,34 @@ async function main() {
     },
   });
 
-  console.log({ user1, user2 });
-  console.log('Seeding successfully ...');
+  // Post 4
+  await prisma.post.upsert({
+    where: { slug: 'follow-nestjs-github' },
+    create: {
+      title: 'Follow Nestjs on GitHub',
+      slug: 'follow-nestjs-github',
+      content: 'https://github.com/nestjs/nest',
+      published: true,
+      author: { connect: { id: user3.id } },
+      categories: {
+        connect: [{ id: category1.id }, { id: category2.id }],
+      },
+    },
+    update: {
+      title: 'Follow Nestjs on GitHub',
+      slug: 'follow-nestjs-github',
+      content: 'https://github.com/nestjs/nest',
+      published: true,
+      author: { connect: { id: user3.id } },
+      categories: {
+        connect: [{ id: category1.id }, { id: category2.id }],
+      },
+    },
+  });
+
+  console.log({ user1, user2, user3 });
+  console.log('------------------------------------------------');
+  console.log('Seeding successfully ...........................');
 }
 
 main()
