a4142d3712b3393734a2ee7a0e067e03255745ba
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-02-05 16:21:34 +0100

    Add example interceptors

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { LoggingInterceptor } from '@common/global-interceptors/logging.interceptor';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
+@UseInterceptors(LoggingInterceptor)
 export class AppController {
   constructor(private appService: AppService) {}
   @Get()
