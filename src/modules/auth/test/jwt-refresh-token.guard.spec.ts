5316a2dae2dcfde81b3a81669a80e1c63fccb7e6
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-02-11 19:28:49 +0100

    fix(auth): remove local guard

---
+++ src/modules/auth/test/jwt-refresh-token.guard.spec.ts
@@ -0,0 +1,10 @@
+import { JwtRefreshTokenGuard } from '../guards';
+
+describe('JwtRefreshTokenGuard', () => {
+  it('Should be defined', () => {
+    const { PostService } = jest.createMockFromModule(
+      '@modules/post/post.service.ts',
+    );
+    expect(new JwtRefreshTokenGuard(new PostService())).toBeDefined();
+  });
+});
